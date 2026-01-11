105622c4d8cf6bd18155009beea0e040914a2a62
Yash-8888 <codingwithcandy@gmail.com>
2026-01-11 12:34:40 +0530

    Nav, Hero, bottem

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,30 @@
+import React, { useEffect, useState } from 'react'
+import './Navbar.css'
+import logo from '../../assets/logo.png'
+
+
+const Navbar = () => {
+  
+    const [Sticky, setSticky] = useState(false);
+    useEffect(() => {
+        window.addEventListener('scroll', () => {
+        window.scrollY > 50 ? setSticky(true) : setSticky(false);
+      });
+    }, []);
+
+  return (
+    <nav className= {`Container ${Sticky ? 'Dark-nav' : ''}`} >
+        <img src={logo} alt="" className="logo" />
+        <ul>
+            <li>Home</li>
+            <li>Program</li>
+            <li>About Us</li>
+            <li>Campus</li>
+            <li>Testamonials</li>
+            <li><button className="btn">Contact Us</button></li>   
+        </ul>
+    </nav>
+  )
+}
+
+export default Navbar
