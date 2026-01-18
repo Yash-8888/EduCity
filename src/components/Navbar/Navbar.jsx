6b4219d08a94e166b50cc0666f5800e830dc7bf3
Yash-8888 <codingwithcandy@gmail.com>
2026-01-18 18:45:05 +0530

    responsive

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react'
 import './Navbar.css'
 import logo from '../../assets/logo.png'
 import { Link } from 'react-scroll'
+import menu_icon from '../../assets/menu-icon.png'
 
 
 const Navbar = () => {
@@ -12,18 +13,23 @@ const Navbar = () => {
         window.scrollY > 50 ? setSticky(true) : setSticky(false);
       });
     }, []);
+    const [mobileMenu, setMobileMenu] = useState(false);
+    const toggelMenu = () => {
+      mobileMenu ? setMobileMenu(false): setMobileMenu(true);
+    }
 
   return (
     <nav className= {`Container ${Sticky ? 'Dark-nav' : ''}`} >
         <img src={logo} alt="" className="logo" />
-        <ul>
+        <ul className={mobileMenu?'':'hide-mobile-menu'}>
             <li><Link to="Hero" smooth={true} offset={0} duration={500}>Home</Link> </li>
-            <li><Link to="Promgrams" smooth={true} offset={0} duration={500}>Program</Link></li>
+            <li><Link to="Prom grams" smooth={true} offset={0} duration={500}>Program</Link></li>
             <li><Link to="About" smooth={true} offset={0} duration={500}>About Us</Link></li>
             <li><Link to="Campus" smooth={true} offset={0} duration={500}>Campus</Link></li>
             <li><Link to="Testimonials" smooth={true} offset={0} duration={500}>Testamonials</Link></li>
             <li><Link to="contact" smooth={true} offset={0} duration={500} className="btn">Contact Us</Link></li>   
         </ul>
+        <img src={menu_icon} alt="" className='Menu-icon' onClick={toggelMenu}/>
     </nav>
   )
 }
